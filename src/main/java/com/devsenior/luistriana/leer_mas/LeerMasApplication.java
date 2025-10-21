44bb2e04be0dba6474e26490831e71fac1d40c48
luis miguel triana rueda <luistriana617@gmail.com>
2025-10-21 16:45:00 -0500

    Initialize Spring Boot project with Maven wrapper and essential configurations

---
+++ src/main/java/com/devsenior/luistriana/leer_mas/LeerMasApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.luistriana.leer_mas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeerMasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeerMasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/luistriana/leer_mas/LeerMasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.luistriana.leer_mas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeerMasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
