cb60f72206e0b85309d848fc1bfc5c4251ca087d
luis miguel triana rueda <luistriana617@gmail.com>
2025-10-28 20:55:09 -0500

    Refactor ModelMapper configuration to enhance null handling and enforce strict matching strategy.

--- src/main/java/com/devsenior/luistriana/leer_mas/config/ModelMapperConfig.java
+++ src/main/java/com/devsenior/luistriana/leer_mas/config/ModelMapperConfig.java
@@ -1,17 +1,21 @@
 package com.devsenior.luistriana.leer_mas.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration // CUANDO EL SERVIDOR ARRANQUE ANOTA ESTA CLASE COMO UNA CLASE DE CONFIGURACION
 public class ModelMapperConfig {
 
-@Bean 
+@Bean // CREA UN BEAN PARA QUE SPRING BOOT LO PUEDA GESTIONAR
 public ModelMapper modelMapper(){
 
 ModelMapper mapper = new ModelMapper();
-mapper.getConfiguration().setSkipNullEnabled(true);
+mapper.getConfiguration().setSkipNullEnabled(true) // IGNORAR CAMPOS NULLOS 
+.setMatchingStrategy(MatchingStrategies.STRICT); // SOLO MAPEO CAMPOS CON NOMBRE EXACTOS
+
 return mapper;
 }
 }
