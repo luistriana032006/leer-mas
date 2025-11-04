5e81ff087bffe5ea5b331b3ef1a8a69d59252a1e
luis miguel triana rueda <luistriana617@gmail.com>
2025-11-03 22:27:09 -0500

    Refactor ModelMapperConfig for improved readability and maintainability; add detailed documentation in preguntas.md explaining ModelMapper usage and configuration.

--- src/main/java/com/devsenior/luistriana/leer_mas/config/ModelMapperConfig.java
+++ src/main/java/com/devsenior/luistriana/leer_mas/config/ModelMapperConfig.java
@@ -2,20 +2,20 @@ package com.devsenior.luistriana.leer_mas.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // CUANDO EL SERVIDOR ARRANQUE ANOTA ESTA CLASE COMO UNA CLASE DE CONFIGURACION
 public class ModelMapperConfig {
 
-@Bean // CREA UN BEAN PARA QUE SPRING BOOT LO PUEDA GESTIONAR
-public ModelMapper modelMapper(){
+    @Bean // CREA UN BEAN PARA QUE SPRING BOOT LO PUEDA GESTIONAR
+    public ModelMapper modelMapper() {
 
-ModelMapper mapper = new ModelMapper();
-mapper.getConfiguration().setSkipNullEnabled(true) // IGNORAR CAMPOS NULLOS 
-.setMatchingStrategy(MatchingStrategies.STRICT); // SOLO MAPEO CAMPOS CON NOMBRE EXACTOS
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setSkipNullEnabled(true) // IGNORAR CAMPOS NULLOS
+                .setMatchingStrategy(MatchingStrategies.STRICT); // SOLO MAPEO CAMPOS CON NOMBRE EXACTOS
 
-return mapper;
-}
+        return mapper;
+    }
 }
